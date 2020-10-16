342dd278987086704279e3e885fd033daf40696d
Shashank Prakash <shashanksharma191098@gmail.com>
2020-10-16 13:26:03 +0530

    log.Fatal() is reolved

--- shashankMongo.go
+++ shashankMongo.go
@@ -187,6 +187,7 @@ func FetchLogin(connectionInfo *ConnectToDataBase, collectionString string, user
     err:= collectionName.FindOne(ctx, filter).Decode(&businessAccount)
 	if err != nil {
 		log.Println(err)
+		return businessAccount, err
 	}
 	resultID = businessAccount.ID.Hex()
 	businessAccount.UserID = resultID
